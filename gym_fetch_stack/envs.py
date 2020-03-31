27cdd6f20628610471159ebef071618d3e61ffee
Eugenio <culurciello@gmail.com>
2020-03-31 14:44:33 -0400

    added 1 bloc env
    from Andre cahng:
    for one block you can add a class FetchStack1TrainerEasyEnv in envs.py

--- gym_fetch_stack/envs.py
+++ gym_fetch_stack/envs.py
@@ -115,6 +115,20 @@ class FetchStack5TrainerOneTenthIsStackingEnv(fetch_stack_env.FetchStackEnv, uti
             initial_qpos=initial_qpos, reward_type=reward_type, goals_on_stack_probability=0.1)
         utils.EzPickle.__init__(self)
 
+class FetchStack1TrainerEasyEnv(fetch_stack_env.FetchStackEnv, utils.EzPickle):
+    def __init__(self, reward_type='incremental'):
+        initial_qpos = {
+            'robot0:slide0': 0.405,
+            'robot0:slide1': 0.48,
+            'robot0:slide2': 0.0,
+            'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+        }
+        fetch_stack_env.FetchStackEnv.__init__(
+            self, 'fetch/stack1.xml', num_blocks=1, block_gripper=False, n_substeps=20,
+            gripper_extra_height=0.2, target_in_the_air=False, target_offset=0.0,
+            obj_range=0.15, target_range=0.15, distance_threshold=DISTANCE_THRESHOLD,
+            initial_qpos=initial_qpos, reward_type=reward_type, goals_on_stack_probability=0.0)
+        utils.EzPickle.__init__(self)
 
 class FetchStack2TrainerEasyEnv(fetch_stack_env.FetchStackEnv, utils.EzPickle):
     def __init__(self, reward_type='incremental'):
